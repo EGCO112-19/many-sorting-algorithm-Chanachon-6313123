7b2b93cc9704fbeb93c3ad2b43577be68f1646c9
Chanachon631312 <chanachon.jan@student.mahidol.edu>
2021-02-12 08:28:10 +0000

    test (get show value N in for loop)

--- simpleMain.c
+++ simpleMain.c
@@ -3,13 +3,23 @@ int main(){
 
   int id ;
   char name[50];
-  
+  int i,N=0,num;
+
   printf("Your Id : ");
   scanf("%d",&id);
 
   printf("Your Name : ");
   scanf("%s",name);
 
+  printf("Get Num : ");
+  scanf("%d",&num);
+
+  
+  for(i=1;i<=num;i++){
+    printf("%d Number\n",i);
+    N+=1;
+    printf("N = %d\n",N);
+  }
 
   return 0;
 }
